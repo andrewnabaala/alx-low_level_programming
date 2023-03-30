a80d40b9d623424f6b471450f2983c389645c12e
andrewnabaala <andrew.k.nabaala@gmail.com>
2023-03-31 00:40:13 +0300

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
     int len;
     list_t *new;
 
-    new = malloc(sizeof(list_t));
+    new = malloc (sizeof(list_t));
     if (new == NULL)
         return (NULL);
 
